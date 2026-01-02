ff71c6c46642e8cdcc252a8b4a36181a4ba29c99
JulioCesarAbreuP <babalawo.espana@gmail.com>
2026-01-02 11:11:59 +0100

    Delete assets/css/js directory

--- assets/css/js/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const links = document.querySelectorAll("nav a");
-  links.forEach(link => {
-    if (link.href === window.location.href) {
-      link.classList.add("activo");
-    }
-  });
-});
